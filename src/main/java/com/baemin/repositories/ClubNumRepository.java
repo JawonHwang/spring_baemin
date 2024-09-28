c4eafae53ecb6c456e5118a7143805449cab06cd
lcy <dldidsla13@naver.com>
2024-09-28 19:33:17 +0900

    기수 테이블 관련 추가

---
+++ src/main/java/com/baemin/repositories/ClubNumRepository.java
@@ -0,0 +1,9 @@
+package com.baemin.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.baemin.domain.entity.ClubNum;
+
+public interface ClubNumRepository extends JpaRepository<ClubNum, Long> {
+
+}
